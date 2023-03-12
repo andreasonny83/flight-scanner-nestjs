e245e0d744bf3983684f039a13469794d13a0523
andreasonny83 <andreasonny83@gmail.com>
2023-03-12 12:37:22 +0000

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,10 @@ import { TerminusModule } from '@nestjs/terminus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FlightsModule } from './flights/flights.module';
-import { SonnyService } from './sonny/sonny.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), FlightsModule, TerminusModule, HttpModule],
   controllers: [AppController],
-  providers: [AppService, SonnyService],
+  providers: [AppService],
 })
 export class AppModule {}
